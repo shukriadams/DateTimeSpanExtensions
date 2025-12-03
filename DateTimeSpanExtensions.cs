520f6655114bd85c1ca3bea8e7c9c2a519440ff5
Shukri Adams <shukri.adams@gmail.com>
2025-12-03 12:30:23 +0100

    typo

--- DateTimeSpanExtensions.cs
+++ DateTimeSpanExtensions.cs
@@ -1,7 +1,7 @@
 using System;
 using System.Globalization;
 
-namespace MadSciebce_DateTimeSpanExtensions
+namespace MadScience_DateTimeSpanExtensions
 {
     public static class DateTimeSpanExtensions
     {
